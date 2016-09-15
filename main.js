3afdeff8b27af09549e0114126bab4976b0a4a87
Robert <rlamacraft@gmail.com>
2016-09-15 18:41:34 +0100

    Reconstructs all posts and replies using tree-like data structure
    Uses a class, forumPost, to build and then render a tree of posts based
    on the post to which they are a reply (their parent) and any posts that
    are replies them (their children).

---
+++ main.js
@@ -0,0 +1,82 @@
+/**
+** Model of a single post, forms the node on a tree of posts and replies
+**/
+class forumPost {
+  constructor(id, parentId, html) {
+    this.id = id;
+    this.parentId = parentId;
+    this.html = html;
+    this.children = [];
+  }
+}
+
+if(window.location.hostname == "forums.theregister.co.uk") {
+  const forumPostsContainer = document.getElementById("forum_posts");
+  const parsedForumPosts = parseForumPosts(forumPostsContainer);
+  renderForumPosts(parsedForumPosts, forumPostsContainer);
+}
+
+/**
+** Parses the HTML of the posts to extract the data necessary to build a tree of all posts and replies
+**/
+function parseForumPosts(forum_posts_container) {
+
+  /* Get the Id of the post, from the id of the permalink anchor tag */
+  function extractId(forumPost_html) {
+    const permalink = forumPost_html.getElementsByClassName("permalink")[0];
+    return( permalink.getAttribute("id").substr(2) ); // id is 'c_#######', so .substr(2) drops the 'c_'
+  }
+
+  /* Get the parent Id of the post, from the in-reply-to link. Returns empty string if root node. */
+  function extractParentId(forumPost_html) {
+    try {
+      const reply_to = forumPost_html.getElementsByClassName("in-reply-to")[0];
+      const reply_to_link = reply_to.getAttribute("href");
+      const reply_to_link_parts = reply_to_link.split("/");
+      return( reply_to_link_parts[reply_to_link_parts.length - 1] );
+    } catch(err) {
+      return("");
+    }
+  }
+
+  forumPosts = {};
+  rootForumPosts = [];
+
+  let forumPosts_html = forum_posts_container.getElementsByClassName("post");
+
+  for (eachForumPost_html of forumPosts_html) {
+    const id = extractId(eachForumPost_html)
+    const parentId = extractParentId(eachForumPost_html);
+
+    const newForumPost = new forumPost(id, parentId, eachForumPost_html);
+    forumPosts[id] = newForumPost;
+    if(parentId != "") {
+      forumPosts[parentId].children.push(newForumPost);
+    } else {
+      rootForumPosts.push(newForumPost);
+    }
+
+  }
+
+  return rootForumPosts;
+}
+
+/**
+** Renders a forumPost onto the page using the original HTML
+**/
+function renderForumPosts(rootForumPosts, forumPostsContainer) {
+
+  /* Recursively render the forumPost and all of its children recursively */
+  function renderPost(post) {
+    let postHTML = post.html;
+    for(eachChildPost of post.children) {
+      postHTML.appendChild(renderPost(eachChildPost));
+    }
+    return(postHTML);
+  }
+
+  forumPostsContainer.innerHTML = "";
+  for (eachRootPost of rootForumPosts) {
+    forumPostsContainer.appendChild(renderPost(eachRootPost));
+  }
+}
